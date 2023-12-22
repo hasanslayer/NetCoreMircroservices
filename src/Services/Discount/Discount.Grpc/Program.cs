46ffa79f012ca37a22f4b09fe1d05183265a95bb
hasanslayer <hasan-dr2010@hotmail.com>
2023-12-22 23:25:00 +0200

    Add getdiscount function with discount service with usage of automapper.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,7 @@
 using Discount.Grpc.Extensions;
+using Discount.Grpc.Mapper;
 using Discount.Grpc.Repositories;
-//using Discount.Grpc.Services;
+using Discount.Grpc.Services;
 
 namespace Discount.Grpc
 {
@@ -15,6 +16,7 @@ namespace Discount.Grpc
 
             // Add services to the container.
             builder.Services.AddGrpc();
+            builder.Services.AddAutoMapper(typeof(DiscountProfile));
             builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
             var app = builder.Build();
@@ -22,7 +24,7 @@ namespace Discount.Grpc
 
 
             // Configure the HTTP request pipeline.
-            //app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<DiscountService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
