1919d85778da439331f4ba4d48f983bba65338ec
hasanslayer <hasan-dr2010@hotmail.com>
2024-03-18 08:24:38 +0200

    Add ocelot caching.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -9,7 +10,7 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 app.UseRouting();
