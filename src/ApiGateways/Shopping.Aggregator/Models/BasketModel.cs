7475a13525638a265f6913b1a94b152deb595d8d
hasanslayer <hasan-dr2010@hotmail.com>
2024-05-25 19:07:38 +0300

    Add dto models for api aggregator.

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string Id { get; set; }
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
