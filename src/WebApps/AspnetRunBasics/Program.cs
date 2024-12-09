9a3631e7f65dbcd2b0347d809b5d290d8fd3884b
Hasan-salim <hasansalimdarwish@gmail.com>
2024-12-09 16:52:24 +0200

    Config Serilog for ElasticSearch and Kibana Sink.

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,4 +1,8 @@
 using AspnetRunBasics.Services;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Net.NetworkInformation;
+using System.Reflection;
 
 namespace AspnetRunBasics
 {
@@ -13,10 +17,23 @@ namespace AspnetRunBasics
             builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
             builder.Services.AddHttpClient<IBasketService, BasketService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
             builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
-
-
             builder.Services.AddRazorPages();
-            
+
+            using var log = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(
+                 new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]))
+                 {
+                     IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{builder.Environment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",
+                     AutoRegisterTemplate = true,
+                     NumberOfShards = 2,
+                     NumberOfReplicas = 1
+                 })
+            .Enrich.WithProperty("Environment",builder.Environment.EnvironmentName)
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
 
             var app = builder.Build();
 
