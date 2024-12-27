41a1813a3c350837006987a1f7180e6f07c2ce03
Hasan-salim <hasansalimdarwish@gmail.com>
2024-12-27 23:39:57 +0200

    Add loggin delegating handler in AspnetRunBasics project.

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -14,10 +14,17 @@ namespace AspnetRunBasics
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddTransient<LoggingDelegatingHandler>();
 
-            builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
-            builder.Services.AddHttpClient<IBasketService, BasketService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
-            builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]));
+            builder.Services.AddHttpClient<ICatalogService, CatalogService>(
+                c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
+            builder.Services.AddHttpClient<IBasketService, BasketService>(
+                c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
+            builder.Services.AddHttpClient<IOrderService, OrderService>(
+                c => c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
             builder.Services.AddRazorPages();
 
             var log = Serilogger.Configure(builder);
