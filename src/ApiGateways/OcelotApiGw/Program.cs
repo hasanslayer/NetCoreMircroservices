7b5bfa04534f229a4cc280ba185512bd93ecbcf2
Hasan Darwish <hassan.darwish@appenza-studio.com>
2025-04-07 13:54:53 +0200

    add serilog to ocelotApiGw.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,16 +1,20 @@
+using Common.Logging;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
+//builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+//builder.Logging.AddConsole();
+//builder.Logging.AddDebug();
 
 builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
+
+builder.Services.AddSerilog(Serilogger.Configure(builder));
 
 var app = builder.Build();
 app.UseRouting();
