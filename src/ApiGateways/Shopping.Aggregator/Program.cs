b75a760229e4db8d20e40702821e7fb80b1c5dde
Hasan Darwish <hassan.darwish@appenza-studio.com>
2025-04-21 05:48:39 +0200

    apply circuit breaker pattern with Polly policies.

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -33,6 +33,8 @@ namespace Shopping.Aggregator
                 .AddHttpMessageHandler<LoggingDelegatingHandler>()
                 .AddTransientHttpErrorPolicy(
                 policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2))
+                ).AddTransientHttpErrorPolicy(
+                policy => policy.CircuitBreakerAsync(5,TimeSpan.FromSeconds(30))
                 );
 
 
